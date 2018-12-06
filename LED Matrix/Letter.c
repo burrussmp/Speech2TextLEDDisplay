f1d61d4eb3c578e67df2e5484bce363e6f9159dd
burrussmp <matthew.p.burruss@vanderbilt.edu>
2018-12-05 23:43:06 -0600

    changes

--- LED Matrix/Letter.c
+++ LED Matrix/Letter.c
@@ -466,15 +466,15 @@ struct letter* createLetter(char l)
         break;
     case '+':
         letter_init(newLetter,5,4);
-        newLetter->pixelMatrix[0][2] = 1;
-        newLetter->pixelMatrix[0][3] = 1;
-        newLetter->pixelMatrix[0][4] = 1;
-        newLetter->pixelMatrix[1][2] = 1;
-        newLetter->pixelMatrix[1][3] = 1;
-        newLetter->pixelMatrix[1][4] = 1;
-        newLetter->pixelMatrix[2][2] = 1;
-        newLetter->pixelMatrix[2][3] = 1;
-        newLetter->pixelMatrix[2][4] = 1;
+        newLetter->pixelMatrix[0][2] = 2;
+        newLetter->pixelMatrix[0][3] = 2;
+        newLetter->pixelMatrix[0][4] = 2;
+        newLetter->pixelMatrix[1][2] = 2;
+        newLetter->pixelMatrix[1][3] = 2;
+        newLetter->pixelMatrix[1][4] = 2;
+        newLetter->pixelMatrix[2][2] = 2;
+        newLetter->pixelMatrix[2][3] = 2;
+        newLetter->pixelMatrix[2][4] = 2;
         break;
     case '2':
         letter_init(newLetter,6,6);
